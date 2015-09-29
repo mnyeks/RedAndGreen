5f714af415441910a6f697a7a21c9898d65d1891
mnyamezeli <you@example.com>
2015-09-29 09:29:57 +0200

    done

--- task3.js
+++ task3.js
@@ -1,11 +1,14 @@
 var redButton = document.getElementById('redButton');
 redbutton.addEventListener('click', function () {
 var block = document.getElementById('block');
+block.innerHTML = "Stop";
 block.className = "red";
 });
 
 var greenbutton = document.getElementById('greenbutton');
 greenbutton.addEventListener('click', function () {
+
 var block = document.getElementById('block');
+block.innerHTML = "Go";
 block.className = "green";
 });
