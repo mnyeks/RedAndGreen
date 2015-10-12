a057b962e50090e1b935932a1c936065c719c5a2
mnyeks <0719075643kennyg@gmail.com>
2015-10-12 12:00:36 +0200

    Delete task4.js

--- task4.js
+++
@@ -1,6 +0,0 @@
-var redbutton = document.getElementById('redbutton');
-redbutton.addEventListener('click', function () {
-var block = document.getElementById('block');
-block.innerHTML = "Stop";
-block.className = "red";
-});
