211c782e395b01f810002c8d0f36c6d1ba92857a
mnyeks <0719075643kennyg@gmail.com>
2015-10-12 13:59:14 +0200

    done

--- task4.js
+++ task4.js
@@ -1,6 +1,20 @@
-var redbutton = document.getElementById('redbutton');
-redbutton.addEventListener('click', function () {
+var button = document.getElementById('button');
+var count = 0;
 var block = document.getElementById('block');
-block.innerHTML = "Stop";
-block.className = "red";
+
+button.addEventListener('click', function() {
+	count += 1;
+	switch(count) {
+		case 1 :
+			block.innerHTML = "STOP";
+			block.className = "red";
+			button.innerHTML = "Make green";
+		break;
+
+        case 2 :
+        	block.innerHTML = "GO";
+			block.className = "green";
+			button.innerHTML = "Make red";
+		break;
+	} 
 });
