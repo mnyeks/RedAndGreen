6632f00fc70aedad3afb7da2d85508679b664318
mnyeks <0719075643kennyg@gmail.com>
2015-10-29 10:44:57 +0200

    task5.js

--- task5.js
+++ task5.js
@@ -3,7 +3,14 @@ var setTestStatus = function(elementid, buttonId, className){
 //Remember that it takes 2 parameters: the id of an element and the test status
 var elem = document.getElementById(elementid);
     elem.className = className;
+var theButton = document.getElementById(buttonId);    
 // change the button text
+if (className === "failing") {
+      button.innerHTML = "Make passing"; 
+   }
+   else {
+   	 button.innerHTML = "Make failing";
+}
 };
 
 //Refresh the page and see what happens.
